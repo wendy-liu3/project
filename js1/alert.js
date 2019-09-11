3bf329fbd2f34255dfde445a3677b74842b0722f
wendy-liu3 <1572152481@qq.com>
2019-09-11 11:30:19 +0800

    试一试

--- js1/alert.js
+++ js1/alert.js
@@ -7,3 +7,9 @@ function a()
     alert ("444444444");
 }
 a();
+var price;
+price=3;
+alert("现在的price是"+price);
+document.write("买好啦");//输出到页面
+var tempMsg = '50年后，我国是否已统一全球？';                                                                                                                        console.log(tempMsg)
+console.log(tempMsg);
